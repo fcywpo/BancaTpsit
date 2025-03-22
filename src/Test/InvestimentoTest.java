d3ca2d7ca33675ae90a3b6c3136d4f5d969bab3b
Francesco <pop.francesco.06@gmail.com>
2025-03-22 18:54:37 +0100

    fix CI per uni

--- src/Test/InvestimentoTest.java
+++ src/Test/InvestimentoTest.java
@@ -1,7 +1,6 @@
 package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 import banca.ContoBancario;
@@ -11,20 +10,21 @@ class InvestimentoTest {
 
   @Test
   void testGeneraRandom() {
-   double min = 1.0, max = 10.0;
-   double risultato = Investimento.GeneraRandom(max,min);
+    double min = 1.0, max = 10.0;
+    double risultato = Investimento.GeneraRandom(max, min);
 
-   assertTrue(risultato >= min && risultato <=max , "Il valore deve essere generato tra min e max");
+    assertTrue(
+        risultato >= min && risultato <= max, "Il valore deve essere generato tra min e max");
   }
 
   @Test
   void testAvviaInvestimento() {
-   ContoBancario conto = new ContoBancario (1000.0, 200.0);
-   Investimento investimento = new Investimento();
+    ContoBancario conto = new ContoBancario(1000.0, 200.0);
+    Investimento investimento = new Investimento();
 
-   asserDoesNotTrhow(() -> investimento.avviaInvestimento(conto));
+    assertDoesNotThrow(() -> investimento.avviaInvestimento(conto));
 
-   assertTrue(conto.getSaldoBanca() >= 0 , "Il saldo non deve essere negativo");
-    assertTrue(conto.getSaldoPortafoglio() >= 0 , "Il saldo non deve essere negativo");
-
+    assertTrue(conto.getSaldoBanca() >= 0, "Il saldo non deve essere negativo");
+    assertTrue(conto.getSaldoPortafoglio() >= 0, "Il saldo non deve essere negativo");
+  }
 }
