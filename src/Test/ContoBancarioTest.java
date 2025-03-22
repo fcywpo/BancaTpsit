f5f0bfd7e1055ba894dc768735639441938e3d9e
Francesco <pop.francesco.06@gmail.com>
2025-03-22 18:35:12 +0100

    UniTest che non mi sono andati

--- src/Test/ContoBancarioTest.java
+++ src/Test/ContoBancarioTest.java
@@ -7,7 +7,22 @@ import org.junit.jupiter.api.Test;
 class ContoBancarioTest {
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void testDeposita() {
+    ContoBancario conto = new ContoBancario(100, 50);
+    conto.deposita(30);
+
+    assertEquals(130, conto.getSaldoBanca(), "Il saldo deve essere aumentato a 130 euro");
+    assertEquals(20, conto.getSaldoBanca(), "Il saldo deve essere diminuito a 20 euro");
+  }
+
+  @Test
+  void testPreleva() {
+    ContoBancario conto = new ContoBancario(100, 50);
+    boolean prelievoEffetuato = conto.preleva(40, false);
+
+    assertTrue(PrelievoEffetuato, "Il prelievo deve essere riuscito");
+
+    assertEquals(60, conto.getSaldoBanca(), "Il saldo deve essere diminuito a 60 euro");
+    assertEquals(90, conto.getSaldoBanca(), "Il saldo deve essere aumentato a 90 euro");
   }
 }
--- src/Test/InvestimentoTest.java
+++ src/Test/InvestimentoTest.java
@@ -4,10 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import banca.ContoBancario;
+import banca.Investimento;
+
 class InvestimentoTest {
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void testGeneraRandom() {
+    double valore = Investimento.GeneraRandom(10, 5);
+    assertTrue(valore >= 5 && valore <= 10, "Il valore generato deve essere compreso tra 5 e 10");
+  }
+
+  @Test
+  void testAvviaInvestimento() {
+    ContoBancario conto = new ContoBancario(1000.0, 500.0);
+    Investimento investimento = new Investimento();
+
+    double saldoIniziale = conto.getSaldoBanca();
+    investimento.avviaInvestimento(conto);
+
+    assertNotEquals(
+        saldoIniziale,
+        conto.getSaldoBanca(),
+        "Il saldo del conto deve cambiare dopo l'investimento");
   }
 }
